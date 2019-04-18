dae15222e4e6ae3b1d2dcd6fecd3b9725954ebc9
sharky <zeebing@gmail.com>
2019-04-18 14:23:54 -0400

    added spotify keys

--- liri.js
+++ liri.js
@@ -1,2 +1,10 @@
 const axios = require("axios");
 
+require("dotenv").config();
+var Spotify = require("node-spotify-api");
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+// console.log(spotify);
